0564a8f042ec145edf68c85244a9418942af3ab8
ed ben <edwardbenedict92@gmail.com>
2021-04-27 17:04:09 +0300

    feat: style for cw

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import InputTodo from "./components/InputTodo";
 import ListTodo from "./components/ListTodo";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <Fragment>
       <div className="container">
+        <Header />
         <InputTodo />
         <ListTodo />
       </div>
